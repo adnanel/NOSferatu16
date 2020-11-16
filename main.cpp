ffcfd02373354b687635ad6dd1fd33105af84c9d
adnanel <adnanel94@gmail.com>
2020-11-16 02:52:18 +0100

    noise program

--- main.cpp
+++ main.cpp
@@ -426,7 +426,7 @@ int main(int argc, char *argv[]) {
 
     NosferatuEmulator emu;
 
-    auto txtProgram = std::ifstream("programs/example.txt");
+    auto txtProgram = std::ifstream("programs/program.txt");
     ProgramReader* reader = new MnemonicReader(txtProgram);
 
 
